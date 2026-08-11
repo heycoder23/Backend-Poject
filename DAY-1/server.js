c47fb7a8311cdecf3d5d308ad6ef3abbe60ab5da
Raj Kumar Gupta <heycoder23@gmail.com>
2026-08-11 13:20:31 +0530

    get and post method add

--- DAY-1/server.js
+++ DAY-1/server.js
@@ -2,9 +2,16 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json())
+ const notes = []
 
+ app.post( '/', (req, res )=>{
+  console.log(req.body);
+  notes.push(req.body)
+    res.send("notes created...?")  
+ } )
 app.get( '/', (req , res)=>{
-    res.send("hello world")
+    res.send(notes)
 });
 
 app.listen(3000, () => {
